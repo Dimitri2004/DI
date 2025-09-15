5fe0e0ee0e4dbc31f0d04025c5c6d7b58c9cda34
dam <daapariciopereira2@danielcastelao.org>
2025-09-15 13:01:49 +0200

    Pruebas con python 0.1

--- Hola.py
+++ Hola.py
@@ -4,13 +4,15 @@ from xmlrpc.client import boolean
 #enteros
 numEntero=3
 
+print(type(numEntero))
 #Coma flotantes
 numFlotante=4.3
 numFlotante1=0.1e-5
 
+print(type(numFlotante))
 #boleanos
 booleano = True #El true es 0 el False es 1
-
+print(type(booleano))
 #complejos
 
 complejo = 2.5 + 9.3j
