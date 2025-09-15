f561857a8d3bf8c697322a2b18025eefd0658fc3
dam <daapariciopereira2@danielcastelao.org>
2025-09-15 12:59:37 +0200

    Pruebas con python

---
+++ Hola.py
@@ -0,0 +1,21 @@
+#numeros
+from xmlrpc.client import boolean
+
+#enteros
+numEntero=3
+
+#Coma flotantes
+numFlotante=4.3
+numFlotante1=0.1e-5
+
+#boleanos
+booleano = True #El true es 0 el False es 1
+
+#complejos
+
+complejo = 2.5 + 9.3j
+
+print(type(complejo))
+
+
+#cadenas
