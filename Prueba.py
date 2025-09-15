a6cd92219d2de93e1ec3c90fd6c35b75aba1ccaf
dam <daapariciopereira2@danielcastelao.org>
2025-09-15 14:11:22 +0200

    Colecciones listas 0.2 practicas con ellas

--- Prueba.py
+++ Prueba.py
@@ -8,6 +8,11 @@
 
 """
 #Listas
-l=[23, 40, 16, -5, 22]
+l=[23, 4.20, 16-3j, "albatros", -5, 22,[34,"Manuel"],4>>1,~5]
 print(type(l))
-print(l)
+
+
+l[4]= "marcelo"
+l[6]=[1,2,3,4]
+
+print(l[6])#puedes hacer que empiece por la derecha con - delante del nombre de la lista
