dcb75840435bcbe7495ef21fc0a6f88edf5916c3
dam <daapariciopereira2@danielcastelao.org>
2025-09-15 14:32:13 +0200

    Colecciones tuplas 0.6 practicas con ellas

--- Prueba.py
+++ Prueba.py
@@ -26,6 +26,6 @@ print(l[::-1])
 
 #Tuplas
 
-t=(2,5,2+3j, "Alberto",(1,3,4,5,6),6,9) #Elementos inmutables dentro de la tupla
-#t(2)=5
+t=(2,5,2+3j, "Alberto",[1,3,4,5,6],6,9) #Elementos inmutables dentro de la tupla
+t[4][3]=2
 print(t)
