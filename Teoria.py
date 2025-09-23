85f38257a66ba2b591b55f54078b0a3e8f2d6188
dam <daapariciopereira2@danielcastelao.org>
2025-09-23 14:07:04 +0200

    Funciones de Objetos inicializados 0.1

--- Teoria.py
+++ Teoria.py
@@ -33,31 +33,4 @@ class Traballador (Posto, Persona):
         super().__init__(tarea,horario,remuneracion,formacion)
         self.NUSS = NUSS
 
-class Persona2:
-    """Clase para definir una persona"""
-    def __init__(self,nome,dni,edade,**outros):
-        self.nome = nome
-        self.dni = dni
-        self.edade = self.comprobarEdade(edade)
-
-    def comprobarEdade(self,edade):
-        if (edade>=0) and edade <100:
-            return edade
-        else:
-            return 0
-
-class Posto2:
-    def __init__(self,tarea,horario,salario,formacion,**outros):
-        self.tarea = tarea
-        self.horario = horario
-        self.salario = salario
-        self.formacion = formacion
-
-class Trabajador2 (Persona2,Posto2):
-    def __init__(self,nome,dni,edade,tarea,horario,salario,formacion,NUSS):
-        super().__init__(nome=nome,dni=dni,edade=edade,tarea=tarea,horario=horario,salario=salario,formacion=formacion)
-        self.NUSS = NUSS
-
-t2 = Trabajador2("Juan",5679,45,"Soldador",7,2300,"CM","13515/UN")
-print(t2.formacion)
 
