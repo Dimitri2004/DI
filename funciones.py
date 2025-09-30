09a038768b2297a6f23cbdf0d896b9dd433d2d58
dam <daapariciopereira2@danielcastelao.org>
2025-09-30 14:30:54 +0200

    Escepciones 0.1

--- funciones.py
+++ funciones.py
@@ -1,7 +1,23 @@
 #Escepciones
 def division (a,b):
     return a/b
+
+n1=1
+n2=str(2) + " Texto"
 try:
-    division(1,0)
+    division(n1,n2)
 except ZeroDivisionError:#recoge escepcion y saca mensaje de error
     print("Operacion abortada, no se puede dividir por cero")
+except TypeError:#error de tipo
+    print("Tipos inadecuados")
+finally:
+    print("Puede que si o no, se realice la division")
+
+
+
+
+try:
+    division(n1,n2)
+except (ZeroDivisionError,TypeError):#recoge doble error en un solo print
+    print("Error al realizar division")
+
