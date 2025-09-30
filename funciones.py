53dcfbe03f90b6f2bba6d79f484ba174b4b0c50a
dam <daapariciopereira2@danielcastelao.org>
2025-09-30 14:38:00 +0200

    Escepciones 0.3

--- funciones.py
+++ funciones.py
@@ -3,7 +3,7 @@ def division (a,b):
     return a/b
 
 n1=1
-n2=str(2) + " Texto"
+n2=3  #str(2) + " Texto"
 try:
     division(n1,n2)
 except ZeroDivisionError:#recoge escepcion y saca mensaje de error
@@ -15,9 +15,11 @@ finally:
 
 
 
-
+resultado = 0
 try:
-    division(n1,n2)
+    resultado = division(n1,n2)
 except (ZeroDivisionError,TypeError) as e:#recoge doble error en un solo print
     print("Error al realizar division "+str(e))
+else:
+    print("El resultado de la operacion es: "+str(resultado))
 
